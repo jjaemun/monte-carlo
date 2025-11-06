075e2f885f5a577109246c7e810842aab3fafe42
jjaemun <jjaemuuu@gmai.com>
2025-11-06 20:05:32 +0100

    implementation

--- include/transforms.hpp
+++ include/transforms.hpp
@@ -2,6 +2,9 @@
 #define TRANSFORMS_IMPLEMENTATION_HPP
 
 
+#include <numbers>
+#include <cmath>
+
 #include <Eigen/Core>
 
 #include "tform.hpp"
@@ -22,7 +25,7 @@ class LinearOperator : public Transform<LinearOperator<T>> {
         }
 
     private:
-        Eigen::ArrayXX<T> &A;
+        const Eigen::ArrayXX<T> &A;
 };
 
 template <typename T>
@@ -40,7 +43,7 @@ class AffineOperator : public Transform<AffineOperator<T>> {
         }
 
     private:
-        Eigen::ArrayXX<T> &A;
+        const Eigen::ArrayXX<T> &A;
         T b;
 };
 
@@ -55,19 +58,45 @@ class BoxMuller : public Transform<BoxMuller<T>> {
             auto ulhs = e.leftCols(mid);
             auto urhs = e.rightCols(mid);
 
-            Eigen::ArrayXX<T> z(e.rows(), e.cols());
+            Eigen::ArrayXX<T> gaussiansamples(e.rows(), e.cols());
 
-            auto zlhs = z.leftCols(mid);
-            auto zrhs = z.rightCols(mid);
+            auto zlhs = gaussiansamples.leftCols(mid);
+            auto zrhs = gaussiansamples.rightCols(mid);
 
             const Eigen::ArrayXX<T> factor = sqrt(-2.0 * log(ulhs));
-            const Eigen::ArrayXX<T> theta = 2.0 * M_PI * urhs;
+            const Eigen::ArrayXX<T> theta = 2.0 * std::numbers::pi_v<T> * urhs;
             
             zlhs = factor * cos(theta);
-            rhs = factor * sin(theta);
+            zrhs = factor * sin(theta);
 
-            return z; 
+            return gaussiansamples; 
         }
 
+        auto backward(Eigen::ArrayXX<T> &e) const -> Eigen::ArrayXX<T> {
+            const auto mid = e.cols() / 2;
+
+            auto zlhs = e.leftCols(mid);
+            auto zrhs = e.rightCols(mid);
+
+            const Eigen::ArrayXX<T> factor = square(zlhs) + square(zrhs);
+            
+            Eigen::ArrayXX<T> uniformsamples(e.rows(), e.cols());
+
+            uniformsamples.leftCols(mid) = exp(-0.5 * factor);
+            for (Eigen::Index i = 0; i < e.rows(); ++i) {
+                for (Eigen::Index j = 0; j < mid; ++j) {
+                    T theta = std::atan2(zrhs(i, j), zlhs(i, j));
+                    if (theta < T(0))
+                        theta += T(2 * std::numbers::pi_v<T>);
+                    
+                    uniformsamples(i, mid + j) = theta / 
+                        T(2 * std::numbers::pi_v<T>);
+                }
+            }
+
+            return uniformsamples;
+        }
+};
+                   
 
 #endif
