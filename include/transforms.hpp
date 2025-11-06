2f5a0b2678af46255dd0ac0c7415b55264d65740
jjaemun <jjaemuuu@gmai.com>
2025-11-06 19:33:47 +0100

    check

--- include/transforms.hpp
+++ include/transforms.hpp
@@ -22,7 +22,7 @@ class LinearOperator : public Transform<LinearOperator<T>> {
         }
 
     private:
-        Eigen::ArrayXX<T> A;
+        Eigen::ArrayXX<T> &A;
 };
 
 template <typename T>
@@ -40,11 +40,34 @@ class AffineOperator : public Transform<AffineOperator<T>> {
         }
 
     private:
-        Eigen::ArrayXX<T> A;
+        Eigen::ArrayXX<T> &A;
         T b;
 };
 
+template <typename T>
+class BoxMuller : public Transform<BoxMuller<T>> {
 
+    public:
+
+        auto forward(Eigen::ArrayXX<T> &e) const -> Eigen::ArrayXX<T> {
+            const auto mid = e.cols() / 2;
+
+            auto ulhs = e.leftCols(mid);
+            auto urhs = e.rightCols(mid);
+
+            Eigen::ArrayXX<T> z(e.rows(), e.cols());
+
+            auto zlhs = z.leftCols(mid);
+            auto zrhs = z.rightCols(mid);
+
+            const Eigen::ArrayXX<T> factor = sqrt(-2.0 * log(ulhs));
+            const Eigen::ArrayXX<T> theta = 2.0 * M_PI * urhs;
+            
+            zlhs = factor * cos(theta);
+            rhs = factor * sin(theta);
+
+            return z; 
+        }
 
 
 #endif
