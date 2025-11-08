09a041d99871bf068bf00e2c79e6eca3ec4a5675
jjaemun <jjaemuuu@gmai.com>
2025-11-08 19:03:48 +0100

    abm

--- include/abm.hpp
+++ include/abm.hpp
@@ -13,9 +13,6 @@
 
 template <typename T>
 class ArithmeticBrownianMotion : Simulator<ArithmeticBrownianMotion<T>> {
-
-    template <Class>
-    using RANDOM_NORMAL_ARRAY = Eigen::Rand::normal<Class>;
     
     template <typename Type>
     using NDARRAY = Eigen::ArrayXX<Type>;
@@ -27,21 +24,25 @@ class ArithmeticBrownianMotion : Simulator<ArithmeticBrownianMotion<T>> {
         ArithmeticBrownianMotion(T spot, T mu, T sigma, INDEX timesteps, INDEX paths) 
          : spot(spot), mu(mu), sigma(sigma), timesteps(timesteps), paths(paths) {}
 
-        template <typename Rng>
-        auto sim(const T s, const T t, Rng rng) -> NDARRAY<T> {
+        template <typename RNG>
+        auto sim(const T s, const T t, RNG rng) -> NDARRAY<T> {
             assert(s < t);
 
             T dt = (t - s) / T(timesteps);
-            auto dw = RANDOM_NORMAL_ARRAY<NDARRAY<T>>(paths, timesteps, rng) 
-                * std::sqrt(dt); 
+            NDARRAY<T> dw = Eigen::Rand::normal<NDARRAY<T>>(
+                paths, timesteps, rng) * std::sqrt(dt); 
              
-            T drift = mu * dt;
-            auto diffusion = sigma * dw;
+            NDARRAY<T> drift = NDARRAY<T>::Constant(paths, timesteps, mu * dt);
+            NDARRAY<T> diffusion = sigma * dw;
 
-            auto ds = drift + diffusion;
-            ds.col(0) = NDARRAY<T>::Constant(paths, 1, spot);
-            
-            return ds.rowwise().cumsum(1);
+            NDARRAY ds = drift + diffusion;
+           
+            NDARRAY<T> s(paths, timesteps + 1);
+            s.col(0) = NDARRAY<T>::Constant(paths, 1, spot);
+            s.block(0, 1, paths, timesteps) = 
+                s.col(0).rowwise() + ds.rowwise().cumsum();
+
+            return S;
     }
          
 
