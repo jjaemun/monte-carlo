14d74a65776daa785dc33c8f9d3ca6cd1cef3814
jjaemun <jjaemuuu@gmai.com>
2025-11-08 19:30:52 +0100

    testing ergonomics

---
+++ include/runtime.hpp
@@ -0,0 +1,29 @@
+#ifndef RUNTIME_HPP
+#define RUNTIME_HPP
+
+#include <EigenRand/EigenRand>
+#include <Eigen/Dense>
+
+
+using INDEX = Eigen::Index;
+
+
+using DYNAMIC = Eigen::Dynamic;
+
+
+template <typename Type>
+using ARRAY1D = Eigen::Array<T, DYNAMIC, INDEX(1)>;
+
+
+template <typename Type>
+using ARRAY2D = Eigen::Array<T, DYNAMIC, DYNAMIC>;
+
+
+template <typename Class>
+using RANDOM_NORMAL_GEN = Eigen::Rand::NormalGen<Class>;
+
+
+using DEFAULT_RNG = Eigen::Rand::Vmt19937_64; 
+
+
+#endif
