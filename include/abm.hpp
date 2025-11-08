8e59720ab5e9fd167d7bd52e193487eab1adeec5
jjaemun <jjaemuuu@gmai.com>
2025-11-08 20:45:25 +0100

    abm sim

--- include/abm.hpp
+++ include/abm.hpp
@@ -2,7 +2,6 @@
 #define ARITHMETIC_BROWNIAN_MOTION_HPP
 
 
-#include <cassert>
 #include <cmath>
 
 #include "simulator.hpp"
@@ -11,15 +10,13 @@
 
 template <typename T>
 class ArithmeticBrownianMotion : public Simulator<ArithmeticBrownianMotion<T>> {
-    
+
     public:
         ArithmeticBrownianMotion(T spot, T mu, T sigma, INDEX timesteps, INDEX paths) 
          : spot(spot), mu(mu), sigma(sigma), timesteps(timesteps), paths(paths) {}
 
         template <typename RNG>
         auto sim(const T s, const T t, RNG &&rng) -> ARRAY2D<T> {
-            assert(s < t);
-
             T dt = (t - s) / T(timesteps);
             auto dw = Eigen::Rand::normal<ARRAY2D<T>>(
                 paths, timesteps, rng) * std::sqrt(dt); 
@@ -30,13 +27,12 @@ class ArithmeticBrownianMotion : public Simulator<ArithmeticBrownianMotion<T>> {
             auto ds = drift + diffusion;
            
             ARRAY2D<T> s(paths, timesteps + 1);
-            s.col(0) = ARRAY1D<T>::Constant(paths, spot);
+            s.col(0) = ARRAY2D<T>::Constant(paths, 1, spot);
             s.block(0, 1, paths, timesteps) = 
                 s.col(0).rowwise() + ds.rowwise().cumsum();
 
-            return S;
+            return s;
     }
-         
 
     private:
         T spot, mu, sigma;
