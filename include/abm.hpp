5364a0bbb72a63fda3f828008085e0a6c96ba544
jjaemun <jjaemuuu@gmai.com>
2025-11-09 17:14:21 +0100

    abm

--- include/abm.hpp
+++ include/abm.hpp
@@ -4,9 +4,9 @@
 
 #include <cmath>
 
-#include "configs.hpp"
 #include "simulator.hpp"
-#include "types.hpp"
+#include "configs.hpp"
+#include "contexts.hpp"
 
 
 
@@ -16,31 +16,28 @@ class ArithmeticBrownianMotion : public Simulator<ArithmeticBrownianMotion<T>> {
     static_assert(std::is_floating_point_v<T>);
 
     public:
-        ArithmeticBrownianMotion(INDEX timesteps, INDEX paths) : 
-            timesteps(timesteps), paths(paths) {
-                samples = RANDOM_NORMAL_GEN<ARRAY2D<T>>(
-                    paths, timesteps, DEFAULT_RNG); 
-        }
+        ArithmeticBrownianMotion(const diffusion::context<T> &ctx, const abm::config<T> &config) 
+            : ctx(ctx), config(config) {}
 
-        ARRAY2D<T> sim(const abm::config<T> &config, const T s, const T t) {
-            T dt = (t - s) / T(timesteps);
+        auto sim(const T s, const T t) -> ARRAY2D<T> {
+            T dt = (t - s) / T(ctx.timesteps);
 
             auto drift = ARRAY2D<T>::Constant(
-                paths, timesteps, config.mu * dt);
-            auto diffusion = config.sigma * samples * std::sqrt(dt);
+                ctx.paths, ctx.timesteps, config.mu * dt);
+            auto diffusion = config.sigma * ctx.nn(0, std::sqrt(dt));
             auto ds = drift + diffusion;
            
-            ARRAY2D<T> s(paths, timesteps + 1);
-            s.col(0) = ARRAY2D<T>::Constant(paths, 1, config.spot);
-            s.block(0, 1, paths, timesteps) = 
+            ARRAY2D<T> s(ctx.paths, ctx.timesteps + 1);
+            s.col(0) = ARRAY2D<T>::Constant(ctx.paths, 1, config.spot);
+            s.block(0, 1, ctx.paths, ctx.timesteps) = 
                 s.col(0).rowwise() + ds.rowwise().cumsum();
 
             return s;
         }
 
     private:
-        INDEX timesteps, paths;
-        ARRAY2D<T> samples;
+        diffusion::context<T> &ctx;
+        abm::config<T> &config;
 };
 
 
