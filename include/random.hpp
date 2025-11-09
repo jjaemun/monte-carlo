e8fe1ca0639028a6c8205c50f6a197ce2d5edcac
jjaemun <jjaemuuu@gmai.com>
2025-11-09 16:55:54 +0100

    cosmetic

--- include/random.hpp
+++ include/random.hpp
@@ -7,21 +7,21 @@
 
 namespace random {
     template <typename T>
-    inline auto poisson(index_t n, index_t m, const double lambda) -> ARRAY2D<T> {
-        return Eigen::Rand::poisson<ARRAY2D<T>>(
-            n, m, detail::default::rng, lambda);
-    }
-
-    template <typename T>
-    inline auto normal(index_t n, index_t m, T mu, T sigma)  -> ARRAY2D<T> {
+    inline auto normal(index_t n, index_t m, T mu, T sigma) noexcept -> ARRAY2D<T> {
         return mu + sigma * Eigen::Rand::normal<ARRAY2D<T>>(
             n, m, detail::default::rng); 
     }
 
     template <typename T>
-    inline auto normal(index_t n, index_t m)  -> ARRAY2D<T> {
+    inline auto normal(index_t n, index_t m) noexcept -> ARRAY2D<T> {
         return Eigen::Rand::normal<ARRAY2D<T>>(
             n, m, detail::default::rng); 
+    }
+
+    template <typename T>
+    inline auto poisson(index_t n, index_t m, T lambda) noexcept -> ARRAY2D<T> {
+        return Eigen::Rand::poisson<ARRAY2D<T>>(
+            n, m, detail::default::rng, lambda);
     }
 
 }
