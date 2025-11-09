2d8fe6f28d84d18e22b0a423ae8c1e7ba29ded5b
jjaemun <jjaemuuu@gmai.com>
2025-11-09 23:32:12 +0100

    overloads

--- include/random.hpp
+++ include/random.hpp
@@ -19,11 +19,28 @@ namespace random {
     }
 
     template <typename T>
+    inline auto normal(index_t n, T mu, T sigma) noexcept -> array1d_t<T> {
+        return mu + sigma * Eigen::Rand::normal<array1d_t<T>>(
+            n, detail::default::rng); 
+    }
+
+    template <typename T>
+    inline auto normal(index_t n) noexcept -> array1d_t<T> {
+        return Eigen::Rand::normal<array1d_t<T>>(
+            n, detail::default::rng); 
+    }
+
+    template <typename T>
     inline auto poisson(index_t n, index_t m, T lambda) noexcept -> array2d_t<T> {
         return Eigen::Rand::poisson<array2d_t<T>>(
             n, m, detail::default::rng, lambda);
     }
 
+    template <typename T>
+    inline auto poisson(index_t n, T lambda) noexcept -> array1d_t<T> {
+        return Eigen::Rand::poisson<array1d_t<T>>(
+            n, m, detail::default::rng, lambda);
+    }
 }
 
 
