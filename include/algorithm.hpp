3293f2b748398df0a72ab2a00376c3ad84350ab1
jjaemun <jjaemuuu@gmai.com>
2025-11-09 17:35:32 +0100

    detail added

--- include/algorithm.hpp
+++ include/algorithm.hpp
@@ -7,17 +7,18 @@
 
 #include "utilities.hpp"
 
-
-template <typename Tuple, std::size_t... Is>
-constexpr auto meta_reverse(Tuple&& tp, std::index_sequence<Is...>) {
-    constexpr std::size_t s = std::tuple_size_v<std::remove_reference_t<Tuple>>;
-    return std::make_tuple(std::get<s - 1 -Is>(std::forward<Tuple>(tp))...);
+namespace detail {
+    template <typename Tuple, std::size_t... Is>
+    constexpr auto reverse(Tuple&& tp, std::index_sequence<Is...>) {
+        constexpr std::size_t s = std::tuple_size_v<std::remove_reference_t<Tuple>>;
+        return std::make_tuple(std::get<s - 1 -Is>(std::forward<Tuple>(tp))...);
+    }
 }
 
 template <typename Tuple>
 constexpr auto reverse(Tuple&& tp) {
     constexpr std::size_t s = std::tuple_size_v<std::remove_reference_t<Tuple>>;
-    return meta_reverse(std::forward<Tuple>(tp), std::make_index_sequence<s>{});
+    return detail::reverse(std::forward<Tuple>(tp), std::make_index_sequence<s>{});
 }
     
 
