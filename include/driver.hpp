d7cc441f301d7f1da6771e77a92372495923bd35
jjaemun <jjaemuuu@gmai.com>
2025-11-09 23:45:41 +0100

    im tired

---
+++ include/driver.hpp
@@ -0,0 +1,28 @@
+#ifndef SIMULATION_DRIVER_HPP
+#define SIMULATION_DRIVER_HPP
+
+
+#include "context.hpp"
+
+
+template <typename... Simulators>
+class SimulationDriver {
+    
+    static_assert(pack_size<Simulators...> > 0);
+
+    public:
+        SimulationDriver(Simulators&&... sims) {
+            sims = { std::make_unique<std::decay_t<Simulators>>(
+                std::forward<Simulators>(sims))...  };
+        }
+
+        void simulate(const context& ctx) {
+
+        }
+            
+
+    
+    private:
+        std::vector<std::unique_ptr<Simulator>> simulators;
+
+#endif 
--- include/simulator.hpp
+++ include/simulator.hpp
@@ -2,8 +2,6 @@
 #define SIMULATOR_HPP
 
 
-#include <type_traits>
-
 #include "utilities.hpp"
 
 
