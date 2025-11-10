b7b63d725f1f079bb8111b453038e4d1a66b597b
jjaemun <jjaemuuu@gmai.com>
2025-11-10 11:59:40 +0100

    revisited

--- include/simulator.hpp
+++ include/simulator.hpp
@@ -5,12 +5,16 @@
 #include "utilities.hpp"
 
 
-template <typename Class>
+template <typename kernel>
 class Simulator {
+
+    template <typename... Args>
+    using Ret = decltype(std::declval<kernel>()(std::declval<Args>()...));
+
     public:
         template <typename... Args>
-        auto simulate(Args&&... args) {
-            return down_cast<Class>(*this).sim(std::forward<Args>(args)...); 
+        auto simulate(Args&&... args) const -> Ret<Args...> {
+            return down_cast<kernel>(*this)(std::forward<Args>(args)...); 
         }
 };
 
