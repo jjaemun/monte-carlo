17b5d1256a5d50ee64100a3d0996d089985902cf
jjaemun <jjaemuuu@gmai.com>
2025-11-13 19:50:30 +0100

    rng

--- include/random.hpp
+++ include/random.hpp
@@ -7,37 +7,37 @@
 
 namespace rng {
     template <typename T>
-    inline auto normal(index_t n, index_t m, T mu, T sigma) noexcept -> array2d_t<T> {
+    inline auto normal(index_t n, index_t m, T mu, T sigma) noexcept {
         return mu + sigma * Eigen::Rand::normal<array2d_t<T>>(
             n, m, detail::defaults::rngen); 
     }
 
     template <typename T>
-    inline auto normal(index_t n, index_t m) noexcept -> array2d_t<T> {
+    inline auto normal(index_t n, index_t m) noexcept {
         return Eigen::Rand::normal<array2d_t<T>>(
             n, m, detail::defaults::rngen); 
     }
 
     template <typename T>
-    inline auto normal(index_t n, T mu, T sigma) noexcept -> array1d_t<T> {
+    inline auto normal(index_t n, T mu, T sigma) noexcept {
         return mu + sigma * Eigen::Rand::normal<array1d_t<T>>(
             n, detail::defaults::rngen); 
     }
 
     template <typename T>
-    inline auto normal(index_t n) noexcept -> array1d_t<T> {
+    inline auto normal(index_t n) noexcept {
         return Eigen::Rand::normal<array1d_t<T>>(
             n, detail::defaults::rngen); 
     }
 
     template <typename T>
-    inline auto poisson(index_t n, index_t m, T lambda) noexcept -> array2d_t<T> {
+    inline auto poisson(index_t n, index_t m, T lambda) noexcept {
         return Eigen::Rand::poisson<array2d_t<T>>(
             n, m, detail::defaults::rngen, lambda);
     }
 
     template <typename T>
-    inline auto poisson(index_t n, T lambda) noexcept -> array1d_t<T> {
+    inline auto poisson(index_t n, T lambda) noexcept {
         return Eigen::Rand::poisson<array1d_t<T>>(
             n, detail::defaults::rngen, lambda);
     }
