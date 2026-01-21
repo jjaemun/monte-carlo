62da40c24168b97bc4ba3d5028b93f27ac1a0b09
jjaemun <jjaemuuu@gmail.com>
2026-01-21 22:57:09 +0100

    random

--- include/random/rng.hpp
+++ include/random/rng.hpp
@@ -2,10 +2,19 @@
 #define RNG_HPP
 
 
+#include <utility>
+
+#include "types.hpp"
+
+
 template <typename Class>
 class RandomNumberGenerator {
     public:
-       RandomNumberGenerator(const Class &rng_) : rng(std::move(rng_)) {}
+        RandomNumberGenerator(const Class &rng) : rng(std::move(rng)) {}
+
+        auto operator()(void) noexcept {
+            return rng();
+        }
 
     private:
         Class rng;
