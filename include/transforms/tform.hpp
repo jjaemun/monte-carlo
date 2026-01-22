3dc84500ae272964f4185f00ebc11472607a2755
jjaemun <jjaemuuu@gmail.com>
2026-01-22 21:51:34 +0100

    tforms

--- include/transforms/tform.hpp
+++ include/transforms/tform.hpp
@@ -1,11 +1,44 @@
 #ifndef TFORM_HPP
-#ifndef TFORM_HPP
+#define TFORM_HPP
+
+#include <ranges>
+#include <type_traits>
+#include <vector>
+
+#include "types.hpp"
 
 
 class Transform {
     public:
-        virtual auto transform(std::vector<f64> &operand) = 0;
-        virtual auto inverse_transform(std::vector<f64> &operand) = 0;
+        virtual void transform(std::vector<f64> &data) = 0;
+        virtual void inverse_transform(std::vector<f64> &data) = 0;
+
+        virtual ~Transform() = default;
+};
+
+
+template <typename... Transforms>
+class Composite final : public Transform {
+    
+    static_assert((std::is_base_of_v<Transform, Transforms> && ...);
+
+    public:
+        explicit Composite(Transforms*... transforms) : transforms{ transforms... } {}
+
+        void transform(std::vector<f64> &data) override {
+            for (auto &tform : transforms) {
+                tform->transform(data);
+            }
+        }
+
+        void inverse_transform(std::vector<f64> &data) override {
+            for (auto &tform : transforms | std::views::reverse) {
+                 tform->inverse_transform(data);
+            }
+        }
+
+    private:
+        std::vector<Transform*> transforms;
 };
 
 
