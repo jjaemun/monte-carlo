3cb6e525b7fed538ffe4c20afdf14931dbd54bf6
jjaemun <jjaemuuu@gmail.com>
2026-01-22 19:34:14 +0100

    gaussian

--- include/random/gaussian.hpp
+++ include/random/gaussian.hpp
@@ -7,23 +7,23 @@
 #include "distribution.hpp"
 
 
-template <typename Class>
+template <typename RandomNumberGenerator>
 class Gaussian : public StatisticalDistribution {
     public:
-        Gaussian(const Class& rng, f64 mu, f64 sigma) 
+        explicit Gaussian(RandomNumberGenerator& rng, f64 mu, f64 sigma) 
             : rng(rng), dist(mu,sigma) {}
 
         auto sample(u64 n) -> std::vector<f64> {
-            std::vector<f64> variates(n);
-            for (auto &i : std::views::iota(0, n)) {
-                variates[i] = dist(rng());
+            std::vector<f64> samples(n);
+            for (auto &sample : samples) {
+                sample = dist(rng());
             }
 
-            return variates;
+            return samples;
         }
 
     private:
-        Class &rng;
+        RandomNumberGenerator &rng;
         std::normal_distribution<f64> dist;
 };
 
