a4984a9a17f54322bf061fa47d13e027bc5e51e2
jjaemun <jjaemuuu@gmail.com>
2026-01-24 10:44:23 +0100

    empirical

--- include/stats/empirical.hpp
+++ include/stats/empirical.hpp
@@ -13,7 +13,7 @@ class SampleMoments : public GenericSampleMoments<SampleMoments> {
     public:
         auto sm(const std::vector<std::vector<f64>> &samples) const {
             std::vector<f64> means{};
-            mean.reserve(samples.size());
+            means.reserve(samples.size());
             for (const auto &timestep : samples) {
                 auto n = static_cast<f64>(timestep.size());
                 auto sum =
@@ -53,22 +53,58 @@ class SampleMoments : public GenericSampleMoments<SampleMoments> {
             const auto paths = samples.front().size();
 
             const auto means = sm(samples);
-            for (auto lagged : std::views::iota(0, timesteps)) {
-                 for (auto concurr : std::views::iota(lagged, timesteps)) {
+            for (auto s : std::views::iota(0, timesteps)) {
+                 for (auto t : std::views::iota(s, timesteps)) {
                     auto sum = (f64)0.0;
                     for (auto path : std::views::iota(0, paths)) 
-                        sum += (samples[lagged][path] - means[lagged]) * 
-                            (samples[concurr][path] - means[concurr]);
+                        sum += (samples[s][path] - means[s]) * 
+                            (samples[t][path] - means[t]);
 
                     const auto cov = sum / (static_cast<f64>(paths) - (f64)1.0);
                     
-                    autocovariance[lagged][concurr] = cov;
-                    autocovariance[concurr][lagged] = cov;
+                    autocovariance[s][t] = cov;
+                    autocovariance[t][s] = cov;
                 }
             }
             
             return autocovariance;
         }
 };
+
+class PearsonAutocorrelation : public GenericPearsonAutocorrelation<PearsonAutocorrelation> {
+    public:
+        auto spa(const std::vector<std::vector<f64>> &samples) const {
+            std::vector<std::vector<f64>> autocorrelation(samples.size());
+            for (auto &v : autocorrelation) {
+                v.resize(samples.size()); 
+            }
+       
+            const auto timesteps = samples.size();
+            const auto paths = samples.front().size();
+
+            const auto variance = empirical.variance(samples);
+            const auto autocovariance = empirical.autocovariance(samples);
+
+            for (auto s : std::views::iota(0, timesteps)) {
+                 for (auto t : std::views::iota(s, timesteps)) {
+                    const auto denom = std::sqrt(variance[s]) * 
+                        std::sqrt(variance[t]);
+
+                    f64 corr{};
+                    if (denom > 0.0)
+                        corr = autocovariance[s][t] / denom; 
+                        
+                    autocorrelation[s][t] = corr;
+                    autocorrelation[t][s] = corr;
+                }
+            }
+
+            return autocorrelation;
+        }
+
+    private:
+        inline SampleMoments empirical{};
+};
+
 
 #endif
