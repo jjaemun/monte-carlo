fe04322a5a7779c1c400019fbc4040ff3c91568c
jjaemun <jjaemuuu@gmail.com>
2026-01-25 13:35:09 +0100

    rngtests

--- tests/random/rngtests.cpp
+++ tests/random/rngtests.cpp
@@ -8,9 +8,9 @@
 #include "random/rng.hpp"
 
 
-namespace default {
+namespace detail {
     static const u64 seed = 0xff;
-} // namespace default.
+} // namespace detail.
 
 
 TEST(rngtest, TrivialRandomGeneration) {
@@ -18,7 +18,7 @@ TEST(rngtest, TrivialRandomGeneration) {
     /* @test TrivialRandomGeneration: trivially checks random
             number yields from rng class. */
 
-    RandomNumberGenerator rng(std::mt19937{default::seed});
+    RandomNumberGenerator rng(std::mt19937{detail::seed});
 
     std::vector<u64> samples((u64)1e4);
     for (auto &sample : samples) {
@@ -32,3 +32,38 @@ TEST(rngtest, TrivialRandomGeneration) {
 
     ASSERT_FALSE(cmpeq);
 }
+
+
+TEST(rngtest, TrivialReproducibility) {
+        
+    /* @test TrivialRandomGeneration: trivially checks behavior
+            is preserved accross seeds. */
+
+    RandomNumberGenerator rng1(std::mt19937{detail::seed});
+    RandomNumberGenerator rng2(std::mt19937{detail::seed});
+
+    bool cmpeq = false;
+    for (auto i : std::views::iota(0, 1e4)) {
+          cmpeq = (rng1() == rng2());
+    }
+
+    ASSERT_TRUE(cmpeq);
+}
+
+
+TEST(rngtest, TrivialIndependence) {
+         
+    /* @test TrivialRandomGeneration: trivially checks behavior
+            is preserved accross seeds. */
+
+    RandomNumberGenerator rng1(std::mt19937{detail::seed});
+    RandomNumberGenerator rng2(std::mt19937{detail::seed + 1});
+
+    bool cmpeq = false;
+    for (auto i : std::views::iota(0, 1e4)) {
+          cmpeq = (rng1() == rng2());
+    }
+
+    ASSERT_FALSE(cmpeq);
+}
+
