93be9a117e45b4758ee32c3f03650884857ecf68
jjaemun <jjaemuuu@gmail.com>
2026-01-25 21:02:45 +0100

    urbg interface

--- include/random/rng.hpp
+++ include/random/rng.hpp
@@ -10,12 +10,18 @@
 template <typename EntropySource>
 class RandomNumberGenerator {
     public:
+        using result_type = typename EntropySource::result_type;
+
+    public:
         explicit RandomNumberGenerator(EntropySource &&src) 
             : src(std::move(src)) {}
 
         auto operator()(void) noexcept {
             return src();
         }
+
+        static constexpr auto min() { return EntropySource::min(); }
+        static constexpr auto max() { return EntropySource::max(); }
 
     private:
         EntropySource src;
