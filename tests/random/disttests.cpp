01926fce3b4ce529f40c8040c6044320e12d0770
jjaemun <jjaemuuu@gmail.com>
2026-01-26 17:19:49 +0100

    distributions

--- tests/random/disttests.cpp
+++ tests/random/disttests.cpp
@@ -17,19 +17,36 @@ namespace detail {
     static constexpr u64 seed = 0xff;
 } // namespace detail.
 
+
 template <typename TestDistribution, typename TheoreticMoments>
-void test(TestDistribution* dist, TheoreticMoments&& theoretic) {
+void test(TestDistribution *dist, TheoreticMoments&& theoretic) {
     SampleDistributionMoments empirical{};
     auto samples = dist->sample((u64)2e6);
 
-    ASSERT_NEAR(empirical.mean(samples), theoretic.mean(*dist), 1e-2);
-    ASSERT_NEAR(empirical.variance(samples), theoretic.variance(*dist), 1e-2);
-    ASSERT_NEAR(empirical.skewness(samples), theoretic.skewness(*dist), 1e-2);
-    ASSERT_NEAR(empirical.kurtosis(samples), theoretic.kurtosis(*dist), 1e-2);
+    ASSERT_NEAR(
+        empirical.mean(samples), 
+        theoretic.mean(*dist), 
+        1e-2
+    );
+    ASSERT_NEAR(
+        empirical.variance(samples), 
+        theoretic.variance(*dist), 
+        1e-2
+    );
+    ASSERT_NEAR(
+        empirical.skewness(samples), 
+        theoretic.skewness(*dist), 
+        1e-2
+    );
+    ASSERT_NEAR(
+        empirical.kurtosis(samples), 
+        theoretic.kurtosis(*dist), 
+        1e-2
+    );
 }
 
 TEST(disttest, StatisticalMomentsCorrectness) {
-    RandomNumberGenerator<std::mt19937> rng{ std::mt19937(detail::seed) };
+    RandomNumberGenerator rng{ std::mt19937(detail::seed) };
     Gaussian gaussian {rng, 0.0, 1.0};
     Poisson poisson{rng, 2.0};
 
