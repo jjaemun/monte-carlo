dff0de5d243d0ac2f75234823082f02809de83d7
jjaemun <jjaemuuu@gmail.com>
2026-02-05 23:13:29 +0100

    first design

--- include/approximations/approx.hpp
+++ include/approximations/approx.hpp
@@ -8,22 +8,20 @@
 
 #include "utitilities.hpp"
 
-
 template <typename... Coeffs>
 class Polynomial {
 
-    static_assert(std::is_arithmetic_v<std::common_type_t<Coeffs...>>);
-
-    private:
-        static constexpr auto degree = pack_size<Coeffs...>;
+    template <typename... Types>
+    using type = std::common_type_t<Types...>;
+    
+    static_assert(std::is_arithmetic_v<type<Coeffs...>>);
 
     public:
         Polynomial(const Coeffs&... coeffs) : coeffs{ coeffs... } {}
 
-        __attribute__((always_inline)) 
-        template <typename T>
-        constexpr T operator()(const T u) const noexcept {
-            auto ret = (T)0.0; 
+        __attribute__((always_inline))
+        constexpr type<Coeffs...> operator()(const type<Coeffs...> u) const noexcept {
+            auto ret = (type<Coeffs...>)0.0; 
             for (const auto &coeff : coeffs | std::views::reverse) 
                ret = std::fma(ret, u, coeff);  
             
@@ -31,30 +29,6 @@ class Polynomial {
         }
 
     private:
-        std::array<std::common_type_t<Coeffs...>, degree> coeffs;
+        std::array<type<Coeffs...>, pack_size<Coeffs...>> coeffs;
 };
 
-template <typename... Coeffs>
-class Polynomial {
-    
-    static_assert(std::is_arithmetic_v<std::common_type_t<Coeffs...>>);
-
-    using type = std::common_type_t<Coeffs...>;
-
-    private:
-        static constexpr auto degree = pack_size<Coeffs...>;
-
-    public:
-        Polynomial(const Coeffs&... coeffs) : coeffs{ coeffs... } {}
-
-        __attribute__((always_inline))constexpr type operator()(const type u) const noexcept {
-            auto ret = (type)0.0; 
-            for (const auto &coeff : coeffs | std::views::reverse) 
-               ret = std::fma(ret, u, coeff);  
-            
-            return ret;
-        }
-
-    private:
-        std::array<type, degree> coeffs;
-}
