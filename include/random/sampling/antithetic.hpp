1cadb52fd781c8b7d9bff0101832bb42d3ff6dfd
jjaemun <jjaemuuu@gmail.com>
2026-02-17 00:09:00 +0100

    antithetic

--- include/random/sampling/antithetic.hpp
+++ include/random/sampling/antithetic.hpp
@@ -6,20 +6,41 @@
 
 
 class AntitheticSampler final : public Sampler {
-    public:
-        explicit AntitheticSampler(StatisticalDistribution *dist) 
-            : dist(dist) {}
+         
+    /**
+     * Constructs pairwise antithetic coupling (Hammersley and Morton, 1956), 
+     * under standard uniform distribution. 
+     */
     
+    private:
+
+       /**
+        * Copies and multiple-construction of distributions are usually 
+        * cheap, in opposite to random engines, which should be initialised 
+        * once per thread (~~rarely, synchronized across them). 
+        */ 
+
+        UniformDistribution *distribution;
+
+       /**
+        * Standard marginal uniformity is trivially satisfied in one-dimensional 
+        * case. 
+        */ 
+
+
+    public:
+        explicit AntitheticSampler(UniformDistribution *distribution) 
+            : distribution(distribution) {}
         std::vector<f64> sample(u64 n) override {
             std::vector<f64> antithetics{};
             antithetics.reserve(n);
 
-            const auto half = n / (u64)2; 
+            const u64 half = n / (u64)2; 
 
-            const auto samples = dist->sample(half);
+            const auto samples = distribution->sample(half);
             for (const auto &sample : samples) {
                 antithetics.push_back(sample);
-                antithetics.push_back(-sample);
+                antithetics.push_back((f64)1.0 - sample);
             }
 
             if (n & 1) 
@@ -28,8 +49,6 @@ class AntitheticSampler final : public Sampler {
             return antithetics;
         }
 
-    private
-        StatisticalDistribution *dist;
 };
 
 #endif 
