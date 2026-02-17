0e4e4d38fc16de755f0198b707e3284232a5c6ee
jjaemun <jjaemuuu@gmail.com>
2026-02-17 18:24:26 +0100

    stratified

---
+++ include/random/sampling/stratified.hpp
@@ -0,0 +1,47 @@
+#ifndef STRATIFIED_HPP
+#define STRATIFIED_HPP
+
+
+#include "sampler.hpp"
+
+
+class StratifiedSampler final : public Sampler {
+         
+    /**
+     * Constructs simple stratified sampler (Hammersley
+     * and Morton, 1956) for variance reduction.
+     */
+
+    public:
+        explicit StratifiedSampler(UniformDistribution *uniform) 
+            : uniform(uniform) {}
+
+        std::vector<f64> sample(u64 n) override {
+ 
+            /**
+             * Partitions the distribution support (unit interval) 
+             * uniformly and samples once per strata. 
+             */           
+
+            const f64 reciprocal = (f64)1.0 / 
+                static_cast<f64>(n); 
+
+            auto samples = uniform->sample(n);
+            for (auto& [i, sample] : std::views::enumerate(samples))
+                sample = (i + sample) * reciprocal;
+
+
+            return samples;
+        }
+    
+    private:
+
+        /**
+         * Copies or multiple-construction of distributions is
+         * usually cheap. 
+        */
+
+        UniformDistribution *uniform;
+};
+
+#endif 
