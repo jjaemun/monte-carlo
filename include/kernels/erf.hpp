75bb83d922ed4b46807fe322d82a1beea4fced2f
jjaemun <jjaemuuu@gmail.com>
2026-02-19 20:23:56 +0100

    erf

---
+++ include/kernels/erf.hpp
@@ -0,0 +1,27 @@
+#ifndef ERF_HPP 
+#define ERF_HPP 
+
+
+#include <cmath>
+#include <limits>
+#include <numbers>
+
+#include "approximation/approx.hpp"
+#include "types.hpp"
+
+
+[[nodiscard]]
+#if defined(__GNUC__) || defined(__clang__)
+__attribute__((hot, always_inline))
+#endif
+constexpr f64 __calerf(const f64 u) noexcept {
+
+    /* 
+          Mathematics of Computation (W. J. Cody, 1969)
+              VOL. 23, NO. 107.
+     
+          ALGO. 715, TOMS (W. J. Cody, 1993) ~SPECFUN
+              VOL. 19, NO. 1.
+     */
+}
+#endif
