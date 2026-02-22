5a01c4d3b409853c80f60360272f19c5b5c3730c
jjaemun <jjaemuuu@gmail.com>
2026-02-22 11:50:43 +0100

    rng

--- include/random/rng.hpp
+++ include/random/rng.hpp
@@ -1,27 +1,27 @@
 #ifndef RNG_HPP
 #define RNG_HPP
 
-
 #include <utility>
 
 #include "types.hpp"
 
 
 template <typename EntropySource>
-class RandomNumberGenerator {
+class RandomBitGenerator {
     public:
         using result_type = typename EntropySource::result_type;
 
     public:
-        explicit RandomNumberGenerator(EntropySource &&src) 
-            : src(std::move(src)) {}
+        template <typename... Args>
+        explicit RandomBitGenerator(Args&&... args) 
+            : src(std::forward<Args>(args)...) {}
 
         auto operator()(void) noexcept {
             return src();
         }
 
-        static constexpr auto min() { return EntropySource::min(); }
-        static constexpr auto max() { return EntropySource::max(); }
+        static constexpr auto min() noexcept { return EntropySource::min(); }
+        static constexpr auto max() noexcept { return EntropySource::max(); }
 
     private:
         EntropySource src;
