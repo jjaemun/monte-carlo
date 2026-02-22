7aa79e5c99966d12b3018b12c616855eae986894
jjaemun <jjaemuuu@gmail.com>
2026-02-22 11:53:46 +0100

    uniform

---
+++ include/random/distributions/uniform.hpp
@@ -0,0 +1,35 @@
+#ifndef UNIFORM_HPP 
+#define UNIFORM_HPP
+
+
+#include <random>
+#include <utility>
+
+#include "distribution.hpp"
+
+
+template <typename RandomBitGenerator>
+class UniformDistribution final {
+    
+    std::uniform_real_distribution<f64> dist;
+
+    public:
+        explicit UniformDistribution(RandomBitGenerator rng, f64 a, f64 b) 
+            : rng(rng), a(a), b(b), dist(a, b) {}
+
+        std::vector<f64> sample(u64 n) override {
+            std::vector<f64> samples(n);
+            for (auto &sample : samples) {
+                sample = dist(rng);
+            }
+
+            return samples;
+        }
+
+    private:
+        RandomBitGenerator rng;
+        f64 a, b;
+};
+
+
+#endif
