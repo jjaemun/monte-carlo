34ff0e36bca7547a39d6270713523815de539158
jjaemun <jjaemuuu@gmail.com>
2026-03-03 00:15:52 +0100

    poisson

--- include/random/distributions/uniform.hpp
+++ include/random/distributions/uniform.hpp
@@ -17,7 +17,7 @@ class Uniform final {
         explicit Uniform(RandomBitGenerator rng, f64 a, f64 b) 
             : rng(rng), a(a), b(b), dist(a, b) {}
 
-        std::vector<f64> sample(u64 n) override {
+        std::vector<f64> sample(u64 n) {
             std::vector<f64> samples(n);
             for (auto &sample : samples) {
                 sample = dist(rng);
