93129eb438dae3514ecd07b0b7ea964c7aba9198
jjaemun <jjaemuuu@gmail.com>
2026-03-03 23:48:27 +0100

    ffffffffaawwwwkkk

--- include/context.hpp
+++ include/context.hpp
@@ -7,37 +7,25 @@
 #include "types.hpp"
 
 
-class Context final {
+struct Context {
+    u64 timesteps;
+    u64 paths;
+}; 
+ 
+
+inline auto timegrid(f64 s, f64 t, f64 delta, u64 timesteps) {
 
     /**
-     * Implements simulation context. Timesteps represents the number
-     * of intervals, so that we actually have tsteps + 1 observations.
+     * Constructs a uniform partition, i.e., linspaced, of the
+     * interval [s, t]. Expects s < t.
      */
 
-    public:
-        Context(u64 timesteps, u64 paths) 
-            : timesteps(timesteps), paths(paths) {}
-                
-        std::vector<f64> timegrid(f64 s, f64 t) const noexcept {
-
-            /**
-             * Constructs a uniform partition, i.e., linspaced, of the
-             * interval [s, t]. Expects s < t.
-             */
-
-            const auto delta = (t - s) / 
-                static_cast<f64>(timesteps);
+    std::vector<f64> times(timesteps + 1);
+    for (auto i : std::views::iota((u64)0, timesteps + 1))
+        times[i] = s + (i * delta);
         
-            std::vector<f64> times(timesteps + 1);
-            for (auto i : std::views::iota((u64)0, timesteps + 1)) {
-                times[i] = s + (i * delta);
-            }
-        
-            return times;
-        } 
-    
-        u64 timesteps, paths;
-}; 
-        
+    return times;
+}
+       
 
 #endif
