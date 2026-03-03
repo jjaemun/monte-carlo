b283a442953488449975cbfdae69c4557493bb70
jjaemun <jjaemuuu@gmail.com>
2026-03-03 23:48:03 +0100

    working...

--- tests/random/disttests.cpp
+++ tests/random/disttests.cpp
@@ -12,8 +12,6 @@
 #include "random/distributions/poisson.hpp"
 #include "random/distributions/uniform.hpp"
 
-#include "random/sampling/antithetic.hpp"
-
 #include "stats/empirical.hpp"
 #include "stats/gaussian.hpp"
 #include "stats/poisson.hpp"
@@ -30,25 +28,25 @@ template <typename Distribution, typename TheoreticMoments>
 void test(Distribution dist, TheoreticMoments&& theoretic) {
     SampleDistributionMoments empirical{};
     auto samples = dist.sample(detail::ssize);
-        
-    for (auto i = 0; i < 11; ++i) {
-        std::cout << samples[i];
-    }
+
     ASSERT_NEAR(
         empirical.mean(samples), 
         theoretic.mean(dist), 
         detail::tol
     );
+
     ASSERT_NEAR(
         empirical.variance(samples), 
         theoretic.variance(dist), 
         detail::tol
     );
+
     ASSERT_NEAR(
         empirical.skewness(samples), 
         theoretic.skewness(dist), 
         detail::tol
     );
+
     ASSERT_NEAR(
         empirical.kurtosis(samples), 
         theoretic.kurtosis(dist), 
@@ -61,15 +59,13 @@ TEST(disttest, StatisticalMomentsCorrectness) {
     /* @test Satatistical...: checkes whether drawn samples converge
             to distribution theoretical moments. */
     
-    //RandomBitGenerator<std::mt19937> rng(detail::seed);
-    Uniform uniform(std::mt19937(detail::seed), 0.0, 1.0);
-    AntitheticSampler antithetic(uniform);
-     
+    RandomBitGenerator<std::mt19937> rng(detail::seed);
+    Uniform uniform(rng, 0.0, 1.0);
 
-    Gaussian gaussian {antithetic, 0.0, 1.0};
-    //Poisson poisson{rng, 2.0};
+    Gaussian gaussian {uniform, 0.0, 1.0};
+    PoissonKnuth poisson {uniform, 2.0};
 
     test(gaussian, GaussianMoments{}); 
-    //test(&poisson, PoissonMoments{}); 
+    test(poisson, PoissonMoments{}); 
 }
 
