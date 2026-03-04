16c513e370382bab00d0169220397cbe7ad26fb6
jjaemun <jjaemuuu@gmail.com>
2026-03-04 22:15:21 +0100

    first idea -- uncivilised

--- include/simulators/gbm.hpp
+++ include/simulators/gbm.hpp
@@ -1,23 +1,55 @@
 #ifndef GEOMETRIC_BROWNIAN_MOTION_HPP
 #define GEOMETRIC_BROWNIAN_MOTION_HPP
 
+#include <cmath>
+#include <utility>
 
-#include "random/gaussian.hpp"
+#include "noise/brownian.hpp"
 #include "simulator.hpp"
 
 
-template <typename RandomNumberGenerator>
-class GeometricBrownianMotion : Simulator {
-        
-        RandomNumberGenerator rng;
+class GeometricBrownianMotion : public Simulator {
+
+    /**
+     * Simulates paths of geometric Brownian motion under the physical
+     * measure. Uses exact discretization. Noise is accordingly Brownian.
+     */
+
+    BrownianProcess brownian;
 
     public:
-        GeometricBrownianMotion(f64 spot, f64 mu, f64 sigma, RandomNumberGenerator& rng) :
-            spot(spot), mu(mu), sigma(sigma), rng(rng) {}
+        explicit GeometricBrownianMotion(u64 timesteps, u64 paths, f64 spot, f64 mu, f64 sigma) 
+                : timesteps(timesteps), paths(paths), spot(spot), mu(mu), sigma(sigma) {}
 
-        std::vector<std::vector<f64>> simulate(const Context &ctx) const override {} 
+        std::vector<std::vector<f64>> simulate(f64 s, f64 t) override {
+            const auto timedelta = (t - s) / 
+                static_cast<f64>(timesteps);
+
+            const auto drift = timedelta * 
+                (mu - (f64)0.5 * sigma * sigma);
+
+            std::vector<std::vector<f64>> process(timesteps + 1);
+            for (auto &timestep : process)
+                timestep.resize(paths)
+
+            // fill spot.
+            std::ranges::fill(process.front(), spot);
+
+            for (auto i : std::views::iota((u64)1, timesteps + 1)) {
+                const auto noise = brownian.increments(paths, timedelta);
+
+                for (auto [current, previous, increment] :
+                    std::views::zip(process[i], process[i - 1], noise)) {
+                    const auto diffusion = sigma * increment;
+                    current = prev * std::exp(drift + diffusion);
+                }
+            }
+
+            return process;
+        } 
 
     private:
+        u64 timesteps, paths;
         f64 spot, mu, sigma;
 };
 
