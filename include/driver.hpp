a6d1d88536a1113c078857d00c516b799f04e67e
jjaemun <jjaemuuu@gmail.com>
2026-03-05 23:43:44 +0100

    first idea -- uncivilised

--- include/driver.hpp
+++ include/driver.hpp
@@ -1,29 +1,36 @@
 #ifndef SIMULATION_DRIVER_HPP
 #define SIMULATION_DRIVER_HPP
 
+#include <memory>
+#include <type_traits>
+#include <vector>
 
-#include "context.hpp"
+#include "simulators/simulator.hpp"
+#include "utilities.hpp"
+
+
+struct results_cache__;
+ 
+    /**
+     * in development... we need a struct to comunicate with pricing and    
+     * plotting layers.
+     */
 
 
 template <typename... Simulators>
 class SimulationDriver {
-
-    //! clearly doesnt work at all crtp X heterogenous containers... just a class sketch.
     
-    static_assert(pack_size<Simulators...> > 0);
+    static_assert((std::is_base_of_v<Simulator, Simulators> && ...));
 
     public:
-        SimulationDriver(Simulators&&... sims) {
-            sims = { std::make_unique<std::common_type<std::decay_t<Simulators>>...>(
-                std::forward<Simulators>(sims))...  };
-        }
+        SimulationDriver(Simulators*... sims) {
+            sims{ std::unique_ptr<Simulator>(sims)... } {}
 
-        void simulate(const context& ctx) {
-
+        void simulate(f64 s, f64 t, results_cache__ *cache) {
+            for (const auto &sim : sims) 
+                cache.push_back(sim->simulate(s, t);
         }
             
-
-    
     private:
         std::vector<std::unique_ptr<Simulator>> sims;
 };
