48524d94b53a5591d609c7220a4b469c1545425e
jjaemun <jjaemuuu@gmail.com>
2026-03-07 20:38:10 +0100

    introspection

--- include/utilities.hpp
+++ include/utilities.hpp
@@ -7,6 +7,8 @@
 template <typename... Types>
 constexpr auto pack_size = sizeof...(Types);
 
+template <typename f, typename... Args>
+using Ret = decltype(std::declval<f>()(std::declval<Args>()...));
 
 
 #endif 
