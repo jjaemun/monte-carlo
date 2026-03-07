342414e30ab0b7780b0bf5d0dca564e41b22b71a
jjaemun <jjaemuuu@gmail.com>
2026-03-07 20:00:38 +0100

    stupid first try

---
+++ plots/simulators/gbm.cpp
@@ -0,0 +1,51 @@
+#include <matplot/matplot.h>
+
+
+#include <vector>
+#include <ranges>
+
+#include "simulators/gbm.hpp"
+#include "stats/empirical.hpp"
+
+
+int main(void) {
+    
+    // params.
+    constexpr u64 timesteps = 1000;
+    constexpr u64 paths = 100;
+
+    constexpr f64 spot = 100.0;
+    constexpr f64 mu = 0.05;
+    constexpr f64 sigma = 0.150;
+
+    constexpr f64  s = 0.0;
+    constexpr f64 t = 1.0;
+
+
+    GeometricBrownianMotion gbm(timesteps, paths, spot, mu, sigma);
+    const auto process = gbm.simulate(s, t);
+
+    // timegrid.
+    auto times = timegrid(s, t, timesteps);
+
+    // init figure.
+    auto f = matplot::figure(true);
+    auto ax = f->current_axes();
+
+    // transpose, plot.
+    for (auto path : std::views::iota((u64)0, paths)) {
+        std::vector<f64> realization(timesteps + 1);
+        for (auto timestep : std::views::iota((u64)0, timesteps + 1))
+            realization[timestep] = process[timestep][path];
+        
+        ax->plot(times, realization)->color({0.7f, 0.7f, 0.7f}).line_width(0.5);
+        ax->hold(true);
+    }
+
+    ax->xlabel("t");
+    ax->ylabel("S(t)");
+    ax->title("Geometric Brownian Motion");
+    f->show();
+
+    return 0;
+}
