25f1814ad302146f656a259083a9bfb99b250410
jjaemun <jjaemuuu@gmail.com>
2026-03-14 17:20:09 +0100

    utils

--- include/utilities.hpp
+++ include/utilities.hpp
@@ -1,14 +1,33 @@
-#ifndef UTILITIES_HPP
-#define UTILITIES_HPP
+#pragma once
 
+
+#include <expected>
 #include <utility>
 
 
 template <typename... Types>
 constexpr auto pack_size = sizeof...(Types);
 
+  /**
+   * Return length of variadic pack. 
+  */
+
+
+template <typename Err>
+[[nodiscard]] auto unex(Err e) noexcept {
+    
+    /**
+     * Constructs std::unexpected from submodule error.
+    */
+
+    return std::unexpected(std::move(e));
+}
+
+
 template <typename f, typename... Args>
 using Ret = decltype(std::declval<f>()(std::declval<Args>()...));
 
-
-#endif 
+  /**
+   * Infers return type given a callable and input 
+   * argument types.
+  */
